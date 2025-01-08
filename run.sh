ab32c3835cde33d9e5111eaa05acb5905362a027
newspace123 <91078730+yinoqifu00@users.noreply.github.com>
2025-01-08 13:44:13 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 /etc/blender0/blender -b -P blender_script_material.py -- \
-        --object_path /home/pjlab/projects/infinigen_sep_part_urdf/outputs/body/body.obj \
+        --object_path xxx.obj \
         --output_dir ./views_body \
         --render_space VIEW \
         #--engine BLENDER_EEVEE_NEXT
